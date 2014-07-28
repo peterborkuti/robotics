8a79fc9b6ee63b53e9f40c7e7f168ee647d6c7f3
Peter Borkuti <peter@debian>
2014-07-28 19:32:52 +0200

    abs encoder - inc encoder with debounce

--- absolute_encoder/abs_encoder/abs_encoder.ino
+++ absolute_encoder/abs_encoder/abs_encoder.ino
@@ -1,12 +1,22 @@
+#include <Encoder.h>
 
 const int sensorPin = 8;     // the number of the QRD1114 output pin
 const int ledPin =  13;      // the number of the LED pin
 
+long incrementalPos  = -999;
+long oldPosition = -999;    // position for debouncing
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+Encoder enc(3,4);
+
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input:
-  pinMode(sensorPin, INPUT);     
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+
+  Serial.begin(9600);
+  Serial.println("Encoder Test:");  
 }
 
 int readQRD1114() {
@@ -20,6 +30,38 @@ void testQRD1114() {
   digitalWrite(ledPin, state == HIGH);
 }
 
+bool readIncEncoder() {
+  long readPosition = enc.read();
+
+  if (oldPosition != readPosition) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    if (incrementalPos != readPosition) {
+      incrementalPos += (incrementalPos < readPosition) ? 1 : -1;
+      enc.write(incrementalPos);
+      oldPosition = incrementalPos;
+
+      return true;
+    }
+  }
+  
+  oldPosition = readPosition;
+
+  return false;
+}
+
+void testIncEncoder() {
+  if (readIncEncoder()) {
+    Serial.println(incrementalPos);
+  }
+  
+}
 void loop(){
-  testQRD1114();
+  //testQRD1114();
+  testIncEncoder();
 }
